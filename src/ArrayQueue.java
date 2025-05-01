60a185d9c08b51df46b5078945284d596b7bfd0e
Omar <kassom771@gmail.com>
2025-05-01 11:34:54 +0100

    Add new class Queue to be completed

---
+++ src/ArrayQueue.java
@@ -0,0 +1,53 @@
+/**
+ * Implementation of the queue ADT using a fixed-length array.
+ */
+public class ArrayQueue<E>{
+
+    private static final int CAPACITY = 10;
+    // instance variables
+    private E[] data; // generic array used for storage
+    private int f = 0; // index of the front element
+    private int sz = 0; // current number of elements
+
+    // constructors
+    public ArrayQueue() {
+        this(CAPACITY);
+    } // constructs queue with default capacity
+
+    public ArrayQueue(int capacity) { // constructs queue with given capacity
+        data = (E[]) new Object[capacity]; // safe cast; compiler may give warning
+    }
+
+    // methods
+
+    /**
+     * Returns the number of elements in the queue.
+     */
+    public int size() {
+
+    }
+
+    /**
+     * Tests whether the queue is empty.
+     */
+    public boolean isEmpty() {
+    }
+
+    /**
+     * Inserts an element at the rear of the queue.
+     */
+    public void enqueue(E e) throws IllegalStateException {
+    }
+
+    /**
+     * Returns, but does not remove, the first element of the queue (null if empty).
+     */
+    public E first() {
+    }
+
+    /**
+     * Removes and returns the first element of the queue (null if empty).
+     */
+    public E dequeue() {
+    }
+}
