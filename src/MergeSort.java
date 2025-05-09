7dafeca208c847baf88a05f4ae183153a76d8499
Omar <kassom771@gmail.com>
2025-05-09 16:16:08 +0100

    Adding classes

---
+++ src/MergeSort.java
@@ -0,0 +1,82 @@
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+
+/**
+ *
+ * @author Rogerio J. Gentil
+ */
+public class MergeSort {
+
+
+    // ----- Array-Based Implementation -----
+    /**
+     * Merge-sort contents of array S.
+     *
+     * @param <K>
+     * @param S
+     * @param comp
+     */
+    public static <K> void mergeSort(K[] S, Comparator<K> comp) {     // merge sorted halves back into original
+    }
+
+    /**
+     * Merge contents of arrays S1 and S2 into properly sized array S.
+     *
+     * @param <K>
+     * @param S1
+     * @param S2
+     * @param S
+     * @param comp
+     */
+    public static <K> void merge(K[] S1, K[] S2, K[] S, Comparator<K> comp) {
+    }
+
+
+    // ----- Queue-Based Implementation -----
+    /**
+     * Merge-sort contents of queue.
+     *
+     * @param <K>
+     * @param S
+     * @param comp
+     */
+    public static <K> void mergeSort(Queue<K> S, Comparator<K> comp) {     // merge sorted halves back into original
+    }
+
+    /**
+     * Merge contents of sorted queues S1 and S2 into empty queue S.
+     *
+     * @param <K>
+     * @param S1
+     * @param S2
+     * @param S
+     * @param comp
+     */
+    public static <K> void merge(Queue<K> S1, Queue<K> S2, Queue<K> S, Comparator<K> comp) {
+    }
+
+    // ----- Bottom-Up (Nonrecursive) Implementation -----
+    /**
+     * Merge-sort contents of data array.
+     *
+     * @param <K>
+     * @param orig
+     * @param comp
+     */
+    public static <K> void mergeSortBottomUp(K[] orig, Comparator<K> comp) {
+    }
+
+    /**
+     * Merges in[start..start+inc−1] and in[start+inc..start+2∗inc−1] into out.
+     *
+     * @param <K>
+     * @param in
+     * @param out
+     * @param comp
+     * @param start
+     * @param inc
+     */
+    public static <K> void merge(K[] in, K[] out, Comparator<K> comp, int start, int inc) {
+}
